2f3a8a2942c9fe738b605e3f91f334481e60269a
John Dickerson <TheSpook@gmail.com>
2014-01-08 17:04:15 -0500

    Parameterized driver
    ArgumentParser implemented in preparation for running in parallel.

--- driver.py
+++ driver.py
@@ -7,6 +7,7 @@ from model import ObjType
 import allocator
 import bounds
 from allocator import DoesNotExistException
+import argparse
 
 import time
 import csv
@@ -41,34 +42,46 @@ def run(num_agents, num_items, dist_type, dup_values, obj_type):
     
     return (sol_exists, build_s, solve_s)
 
-if __name__ == '__main__':
 
-    # Distribution of valuations we want to use
-    dist_type = DistTypes.polya_urn_real
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Find envy-free allocations.')
+    parser.add_argument("-f", "--filename", dest="filename", required=True,
+                      metavar="FILE", help="write output to FILE")
+    parser.add_argument("-r", "--num_repeats", type=int, dest="num_repeats", default=10,
+                      help="num repeat runs per parameter setting")
+    parser.add_argument("-n", type=int, nargs=3, dest="N", default=(5,6,1),
+                      help="range(a,b,c) iterating over num agents")
+    parser.add_argument("-m", type=int, nargs=3, dest="M", default=(5,10,1),
+                      help="range(a,b,c) iterating over num items")
+    parser.add_argument("--obj-feas", action="store_const", const=ObjType.feasibility, dest="obj_type",
+                      help="Objective function: feasibility")
+    parser.add_argument("--obj-social", action="store_const", const=ObjType.social_welfare_max, dest="obj_type",
+                      help="Objective function: max social welfare")
+    parser.add_argument("--dist-urand-int", action="store_const", const=DistTypes.urand_int, dest="dist_type")
+    parser.add_argument("--dist-urand-real", action="store_const", const=DistTypes.urand_real, dest="dist_type")
+    parser.add_argument("--dist-zipf-real", action="store_const", const=DistTypes.zipf_real, dest="dist_type")
+    parser.add_argument("--dist-polya-urn-real", action="store_const", const=DistTypes.polya_urn_real, dest="dist_type")
+    args = parser.parse_args()
+
 
     # How to handle duplicate valuations for different items by the same agent?
     dup_values = DupValues.allowed
 
-    # Objective: social welfare maximization or just feasibility?
-    obj_type = ObjType.feasibility #ObjType.social_welfare_max
-
     # Write one row per run, or one row per N runs (aggregate)?
     write_all = True
 
-    # How many repeat runs per parameter vector?
-    N = 100
-
-    print "Running with dist_type={0}, dup_values={1}, obj_type={2}".format(dist_type, dup_values, obj_type)
-    with open("out_{0}.csv".format(current_ms_time()), 'wb') as csvfile:
+    with open(args.filename, 'wb') as csvfile:
 
         # Write overall stats to out.csv
         writer = csv.writer(csvfile, delimiter=',')
 
-        for num_agents in range(5,26,5):
+        for num_agents in range(args.N[0], args.N[1], args.N[2]):
 
             # Phase transition plots runtime, %feas vs. #items
-            for num_items in range(num_agents,100,1):
-            #for num_items in range(99,num_agents,-1):
+            for num_items in range(args.M[0], args.M[1], args.M[2]):
+            
                 # Never feasible if fewer items than agents
                 if num_items < num_agents:
                     continue
@@ -77,10 +90,10 @@ if __name__ == '__main__':
                 build_s_min = solve_s_min = 10000.0
                 build_s_max = solve_s_max = -1.0
                 sol_exists_accum = 0
-                for _ in xrange(N):
+                for _ in xrange(args.num_repeats):
 
                     # Generate an instance and solve it; returns runtime of IP write+solve
-                    sol_exists, build_s, solve_s = run(num_agents, num_items, dist_type, dup_values, obj_type)
+                    sol_exists, build_s, solve_s = run(num_agents, num_items, args.dist_type, dup_values, args.obj_type)
 
                     # Maintain stats on the runs
                     sol_exists_accum += 1 if sol_exists else 0
@@ -97,23 +110,27 @@ if __name__ == '__main__':
 
                     # If we're recording ALL data, write details for this one run
                     if write_all:
-                        writer.writerow([num_agents, num_items, dist_type, N, obj_type, sol_exists, build_s, solve_s])
+                        writer.writerow([num_agents, num_items, args.dist_type, args.num_repeats, args.obj_type, sol_exists, build_s, solve_s])
                         csvfile.flush()
 
                 # Report stats over all N runs, both to stdout and to out.csv
-                build_s_avg = build_s_accum / N
-                solve_s_avg = solve_s_accum / N
+                build_s_avg = build_s_accum / args.num_repeats
+                solve_s_avg = solve_s_accum / args.num_repeats
 
                 print "Build Avg: {0:3f}, Min: {1:3f}, Max: {2:3f}".format(build_s_avg, build_s_min, build_s_max)
                 print "Solve Avg: {0:3f}, Min: {1:3f}, Max: {2:3f}".format(solve_s_avg, solve_s_min, solve_s_max)
-                print "N={0}, M={1}, fraction feasible: {2} / {3}".format(num_agents, num_items, sol_exists_accum, N)
+                print "N={0}, M={1}, fraction feasible: {2} / {3}".format(num_agents, num_items, sol_exists_accum, args.num_repeats)
 
                 # If we're only writing aggregate data, write that now
                 if not write_all:
-                    writer.writerow([num_agents, num_items, dist_type, N, obj_type,
+                    writer.writerow([num_agents, num_items, 
+                                     args.dist_type, args.num_repeats, args.obj_type,
                                      sol_exists_accum, 
                                      build_s_avg, build_s_min, build_s_max,
                                      solve_s_avg, solve_s_min, solve_s_max,
                                      ])
                 csvfile.flush()
 
+
+if __name__ == '__main__':
+    main()
