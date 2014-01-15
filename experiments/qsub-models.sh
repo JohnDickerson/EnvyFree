34c24810f0dce18436ea81a4023bb8ed45c6e94a
John Dickerson <TheSpook@gmail.com>
2014-01-15 18:45:58 -0500

    Model 1 vs 2 Experiments

---
+++ experiments/qsub-models.sh
@@ -0,0 +1,74 @@
+#!/bin/csh
+
+###### Number of CPUs MUST be a multiple of 16
+#PBS -l ncpus=160
+#PBS -l walltime=8:00:00
+
+###### combine stderr and stdout to one file
+#PBS -j oe
+
+######
+#PBS -q batch
+
+###### Need to export CPLEX Python API path
+#PBS -v PYTHON_PATH
+
+set echo
+ja $HOME/ja.$PBS_JOBID
+
+###### Scratch is a testbed, all files auto-deleted after >7 days!
+cd $SCRATCH
+
+# Copy necessary files (all .py files) to $SCRATCH
+cp $HOME/code/EnvyFree/trunk/*.py .
+
+# Make sure we're running the right Python ...
+source /usr/share/modules/init/csh
+module load python/2.7.3
+
+# Start seed value -- we increment as we use it
+set SEED = `date +%s`
+# Number of repeat random runs per parameter vector setting
+set NUM_REPEATS = 1
+# Objective: either --obj-feas or --obj-social
+#set OBJECTIVE = "--obj-feas"  # set via outer script
+# Utility distribution: --dist-urand-int, --dist-urand-real, --dist-zipf-real
+#                       --dist-polya-urn-real, or --dist-correlated-real
+#set DISTRIBUTION = "--dist-urand-real"   # set via outer script
+# Iterate over #agents:  range($N_MIN, $N_MAX, $N_STEP)
+#set N_MIN = 3  # set via outer script
+#set N_MAX = 10 # set via outer script 
+set N_STEP = 1
+# Iterate over #items:  range($M_MIN, $M_MAX, $M_STEP)
+#                       (the Python code will skip #items<#agents)
+set M_MIN = 3
+set M_MAX = 100
+set M_STEP = 1
+
+# Start one big serial Python loop on each blade
+set COUNTER = 0
+set MAX = 80    # CHECK:  MAX <= 1/8 of number of CPUs requested
+while ( $COUNTER < $MAX )
+
+    # Want a different seed for each of the instances
+    #set SEED=`dd if=/dev/urandom count=1 2> /dev/null | cksum | cut -f1 -d" "`
+    @ SEED++
+
+    # Runtime output will be stored here
+    set OUTBASE=out${OBJECTIVE}_${DISTRIBUTION}_${N_MIN}_${SEED}
+
+    # Run the Python script for E-F allocations (background process!)
+    # Run same seed for Model #1 and Model #2
+    set OUTFILE=${OUTBASE}a.csv
+    python driver.py --filename $OUTFILE --num_repeats $NUM_REPEATS $OBJECTIVE $DISTRIBUTION --seed $SEED -n $N_MIN $N_MAX $N_STEP -m $M_MIN $M_MAX $M_STEP  &
+    set OUTFILE=${OUTBASE}b.csv
+    python driver.py --filename $OUTFILE --num_repeats $NUM_REPEATS $OBJECTIVE $DISTRIBUTION --seed $SEED -n $N_MIN $N_MAX $N_STEP -m $M_MIN $M_MAX $M_STEP --alternate-IP-model &
+    @ COUNTER++
+end
+
+# Wait for all our little serial children to finish
+wait
+
+#cp $SCRATCH/*.csv $HOME/
+
+ja -chlst $HOME/ja.$PBS_JOBID
